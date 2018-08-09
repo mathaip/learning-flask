3cb8d3d2de76cc4eeee6c095e058682345538aad
Mathai Puthiakunnel <mathaiputhiakunnel@Mathais-MacBook-Air.local>
2018-08-09 14:51:21 +0530

    Going to push to Heroku and Git

--- routes.py
+++ routes.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import User, Place
+from models import db
+from forms import SignupForm, LoginForm, AddressForm
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/learningflask"
+db.init_app(app)
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -8,6 +16,73 @@ def index():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+	if 'email' in session:
+		return redirect(url_for('home'))
+	form = SignupForm()
+
+	if request.method == "POST":
+		if form.validate()==False:
+			return render_template("signup.html", form=form)
+		newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+		db.session.add(newuser)
+		db.session.commit()
+		return redirect(url_for("home"))
+
+	elif request.method == "GET":
+		return render_template("signup.html", form = form)
+@app.route("/home", methods=['POST','GET'])
+def home():
+	if 'email' not in session:
+		return redirect(url_for('login'))
+
+
+	form= AddressForm()
+	places=[]
+	my_coordinates = (37.4221, -122.0844)
+
+	if request.method == 'POST':
+		if form.validate()==False:
+			return render_template('home.html', form=form)
+		else:
+			address = form.address.data
+			p = Place()
+			my_coordinates = p.address_to_latlng(address)
+			places = p.query(address)
+			return render_template("home.html", form=form, my_coordinates=my_coordinates, places=places)
+	elif request.method == 'GET':
+		return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+	if 'email' in session:
+		return redirect(url_for('home'))
+	form=LoginForm()
+	if request.method=="POST":
+		if form.validate()== False:
+			return render_template("login.html",form=form)
+		else:
+			email = form.email.data
+			password = form.password.data
+
+			user = User.query.filter_by(email=email).first()
+			if user is not None and user.check_password(password):
+				session['email']= form.email.data
+				return redirect(url_for('home'))
+			else:
+				return redirect(url_for('login'))
+	else: 
+		 request.method == "GET"
+		 return render_template('login.html', form=form)
+@app.route("/logout")
+def logout():
+  session.pop('email', None)
+  return redirect(url_for('login'))
+
+
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
